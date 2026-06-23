17ebf4ce7f4d08d1ed632f251f571c8f766942e9
Zakk Heile <zakk@valdese.net>
2026-06-23 13:05:57 -0400

    Standalone app

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
   base: "./",
   server: {
     host: "0.0.0.0",
